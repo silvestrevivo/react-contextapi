16439ba6ea01b4ed78619df6b6996e3f3a4029bb
silvestrevivo <silvestrevivomillan@gmail.com>
2018-09-04 12:08:32 +0200

    axios and environment variables added

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Axios from 'axios'
 
 const Context = React.createContext()
 
@@ -7,6 +8,14 @@ export class Provider extends Component {
     track_list: [{ track: { track_name: 'abc' } }, { track: { track_name: '123' } }],
     heading: 'top 10 Tracks',
   }
+
+  componentDidMount() {
+    axios
+      .get()
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err))
+  }
+
   render() {
     return <Context.Provider value={this.state}>{this.props.children}</Context.Provider>
   }
