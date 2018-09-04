1f7c189139527f45f371248d7fcbfd24b52cb7ca
silvestrevivo <silvestrevivomillan@gmail.com>
2018-09-04 14:43:35 +0200

    displaying data with bootstrap

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Consumer } from '../../context'
+import Spinner from '../layout/Spinner'
+import Track from './Track'
 
 class Tracks extends Component {
   render() {
@@ -7,7 +9,21 @@ class Tracks extends Component {
       <Consumer>
         {value => {
           console.log('value from Consumer', value)
-          return <h1>Tracks</h1>
+          const { track_list, heading } = value
+          if (track_list === undefined || track_list.length === 0) {
+            return <Spinner />
+          } else {
+            return (
+              <React.Fragment>
+                <h3 className="text-center mb-4">{heading}</h3>
+                <div className="row">
+                  {track_list.map(item => {
+                    return <Track key={item.track.track_id} track={item.track} />
+                  })}
+                </div>
+              </React.Fragment>
+            )
+          }
         }}
       </Consumer>
     )
